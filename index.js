86714ea11ec7e377f82a7314645a74e7ad3ebf65
Felipe <joandersonfsbarbosa@gmail.com>
2023-08-07 12:29:50 -0300

    adding cors

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const cors = require("cors");
 
 const app = express();
 
